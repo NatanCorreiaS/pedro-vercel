8ada2fc11982ff633485307c431c5f83a9a4d08a
Natan <natancorreiatr@gmail.com>
2024-11-23 20:46:37 -0300

    cansaço

--- index.mjs
+++ index.mjs
@@ -20,19 +20,19 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("armazem_database").command({ ping: 1 });
-    console.log("Conectado ao mongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("armazem_database").command({ ping: 1 });
+//     console.log("Conectado ao mongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 app.use(express.json());
 
